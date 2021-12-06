1183ac8a35088e739a9bb738cb9fb40c45b14bba
Zach Carter <carter.peter.zach1@gmail.com>
2021-12-05 20:08:42 -0800

    completed history display

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ function multiply (num1, num2){
 }
 
 function divide (num1, num2){
+    if (num2 === 0){
+        return 'ERROR';
+    }
     return num1 / num2;
 }
 
@@ -43,6 +46,7 @@ console.log(operate(0, 2, 2));
 
 //elements
 const display = document.querySelector('.screen-text');
+const history = document.querySelector('.operations');
 const numberButtons = document.querySelectorAll('.number-button');
 const equalButton = document.querySelector('.equals-button');
 const operators = document.querySelectorAll('.operator-button');
@@ -69,21 +73,17 @@ numberButtons.forEach(button =>{
             }
             else if (!num1Input){
                 num2 = displayValue
-                console.log(`num1: ${num1}`);
-                console.log(`num2: ${num2}`);
+                history.textContent += ` ${num2}`
             }
         }
         else if (display.textContent.length < 9){
             displayValue += event.target.textContent;
             if (num1Input){
                 num1 = displayValue
-                console.log(`num1: ${num1}`);
-                console.log(`num2: ${num2}`);
             }
             else if (!num1Input){
                 num2 = displayValue
-                console.log(`num1: ${num1}`);
-                console.log(`num2: ${num2}`);
+                history.textContent += ` ${num2}`
             }
         }
         else {
@@ -101,22 +101,22 @@ operators.forEach(button => {
         switch (event.target.classList[0]){
             case 'plus-button': {
                 operator = 0;
-                console.log(`operator: ${operator}`);
+                history.textContent = `${num1} +`;
                 break;
             }
             case 'minus-button': {
                 operator = 1;
-                console.log(operator);
+                history.textContent = `${num1} -`
                 break;
             }
             case 'times-button': {
                 operator = 2;
-                console.log(operator);
+                history.textContent = `${num1} x`
                 break;
             }
             case 'divide-button': {
                 operator = 3;
-                console.log(operator);
+                history.textContent = `${num1} \u00F7`
                 break;
             }
             
@@ -125,9 +125,9 @@ operators.forEach(button => {
 })
 
 equalButton.addEventListener('click', () =>{
-    let int1 = parseInt(num1);
-    let int2 = parseInt(num2);
-    result = operate(operator, int1, int2);
+    let float1 = parseFloat(num1);
+    let float2 = parseFloat(num2);
+    result = operate(operator, float1, float2);
     display.textContent = result;
     clear();
 })
