94b6e6b3a0925c8b4c36d116f4cf384390e632d5
Zach Carter <carter.peter.zach1@gmail.com>
2021-12-06 20:26:42 -0800

    completed stringing together operations, double digit inputs do not work properly

--- script.js
+++ script.js
@@ -40,9 +40,8 @@ function clear(){
     num2 = 0;
     displayValue = 0;
     num1Input = true;
+    history.textContent = '';
 }
-
-console.log(operate(0, 2, 2));
 
 //elements
 const display = document.querySelector('.screen-text');
@@ -60,16 +59,17 @@ let operator;
 display.textContent = displayValue;
 
 let num1Input = true;
+let inputHistory = [];
 
 //add event listener to all number buttons and update the display value
 numberButtons.forEach(button =>{
     button.addEventListener('click', (event) => {
+        inputHistory.push(event.target.classList[1]);
         if (displayValue === 0){
             displayValue = event.target.textContent;
             if (num1Input){
+                result = 0;
                 num1 = displayValue
-                console.log(`num1: ${num1}`);
-                console.log(`num2: ${num2}`);
             }
             else if (!num1Input){
                 num2 = displayValue
@@ -79,6 +79,7 @@ numberButtons.forEach(button =>{
         else if (display.textContent.length < 9){
             displayValue += event.target.textContent;
             if (num1Input){
+                result = 0;
                 num1 = displayValue
             }
             else if (!num1Input){
@@ -93,29 +94,147 @@ numberButtons.forEach(button =>{
     })
 })
 
+function equals (number1, number2, math){
+    let float1 = parseFloat(number1);
+    let float2 = parseFloat(number2);
+    result = operate(math, float1, float2);
+    display.textContent = result;
+    return result;
+}
+
 operators.forEach(button => {
     button.addEventListener('click', (event) => {
         displayValue = 0;
         num1Input = false;
-        
+        inputHistory.push(event.target.classList[0]);
+        let previousOperator = inputHistory[inputHistory.length - 3];
         switch (event.target.classList[0]){
             case 'plus-button': {
                 operator = 0;
+                if (previousOperator === 'plus-button'){
+                    equals(num1, num2, operator);
+                    num1 = result;
+                    num1Input = false;
+                }
+                else if (previousOperator === 'divide-button' || previousOperator === 'times-button' || previousOperator === 'minus-button'){
+                    
+                    switch (previousOperator){
+                        case 'minus-button': {
+                            equals(num1, num2, 1);
+                            num1 = result;
+                            num1Input = false;
+                            break;
+                        }
+                        case 'times-buton': {
+                            equals(num1, num2, 2);
+                            num1 = result;
+                            num1Input = false;
+                            break;
+                        }
+                        case 'divide-button': {
+                            equals(num1, num2, 3);
+                            num1 = result;
+                            num1Input = false;
+                        }
+                    }
+                }
                 history.textContent = `${num1} +`;
                 break;
             }
             case 'minus-button': {
                 operator = 1;
+                if (previousOperator === 'minus-button'){
+                    equals(num1, num2, operator);
+                    num1 = result;
+                    num1Input = false;
+                }
+                else if (previousOperator === 'plus-button' || previousOperator === 'times-button' || previousOperator === 'divide-button'){
+                    console.log(`num1: ${num1}, result: ${result}`);
+                    switch (previousOperator){
+                        case 'plus-button': {
+                            equals(num1, num2, 0);
+                            num1 = result;
+                            num1Input = false;
+                            break;
+                        }
+                        case 'times-buton': {
+                            equals(num1, num2, 2);
+                            num1 = result;
+                            num1Input = false;
+                            break;
+                        }
+                        case 'divide-button': {
+                            equals(num1, num2, 3);
+                            num1 = result;
+                            num1Input = false;
+                        }
+                    }
+                }
                 history.textContent = `${num1} -`
                 break;
             }
             case 'times-button': {
                 operator = 2;
-                history.textContent = `${num1} x`
+                if (previousOperator === 'times-button'){
+                    equals(num1, num2, operator);
+                    num1 = result;
+                    num1Input = false;
+                }
+                else if (previousOperator === 'plus-button' || previousOperator === 'divide-button' || previousOperator === 'minus-button'){
+                    console.log(previousOperator);
+                    switch (previousOperator){
+                        case 'plus-button': {
+                            equals(num1, num2, 0);
+                            num1 = result;
+                            num1Input = false;
+                            break;
+                        }
+                        case 'minus-button': {
+                            console.log(`num1: ${num1}, num2: ${num2}`);
+                            equals(num1, num2, 1);
+                            num1 = result;
+                            num1Input = false;
+                            break;
+                        }
+                        case 'divide-button': {
+                            equals(num1, num2, 3);
+                            num1 = result;
+                            num1Input = false;
+                        }
+                    }
+                }
+                history.textContent = `${num1} x`;
                 break;
             }
             case 'divide-button': {
                 operator = 3;
+                if (previousOperator === 'divide-button'){
+                    equals(num1, num2, operator);
+                    num1 = result;
+                    num1Input = false;
+                }
+                else if (previousOperator === 'plus-button' || previousOperator === 'times-button' || previousOperator === 'minus-button'){
+                    switch (previousOperator){
+                        case 'plus-button': {
+                            console.log('I should add previous two numbers');
+                            equals(num1, num2, 0);
+                            num1 = result;
+                            num1Input = false;
+                            break;
+                        }
+                        case 'minus-buton': {
+                            equals(num1, num2, 1);
+                            num1 = result;
+                            num1Input = false;
+                            break;
+                        }
+                        case 'times-button': {
+                            equals(num1, num2, 2);
+                            num1 = result;
+                            num1Input = false;
+                        }
+                    }
+                }
                 history.textContent = `${num1} \u00F7`
                 break;
             }
@@ -125,16 +244,17 @@ operators.forEach(button => {
 })
 
 equalButton.addEventListener('click', () =>{
-    let float1 = parseFloat(num1);
-    let float2 = parseFloat(num2);
-    result = operate(operator, float1, float2);
-    display.textContent = result;
+    equals(num1, num2, operator);
     clear();
+    num1 = result;
+    lastKeyPressed = 'equals-button';
 })
 
 clearButton.addEventListener('click', () => {
     clear();
     display.textContent = displayValue;
+    lastKeyPressed = 'clear';
+    inputHistory = [];
 })
 
 
